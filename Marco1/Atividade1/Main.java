d80b815be6fa9968620a6472fce07fdf5083e8f8
Maria Luiza Ribeiro Rocha <106209553+Mariarocha0@users.noreply.github.com>
2023-05-10 04:58:17 +0000

    Construção da Main

--- Marco1/Atividade1/Main.java
+++ Marco1/Atividade1/Main.java
@@ -1,5 +1,29 @@
 package Marco1.Atividade1;
 
-public class Main{
-    
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.println(" Cálculo Ponto Cartesiano ");
+            double x1, x2, y1, y2 = 0.00;
+            System.out.print("Insira o valor de x1: ");
+            x1 = entrada.nextDouble();
+            System.out.print("Insira o valor de y1: ");
+            y1 = entrada.nextDouble();
+
+            System.out.print("Insira o valor de x2: ");
+            x2 = entrada.nextDouble();
+            System.out.print("Insira o valor de y2: ");
+            y2 = entrada.nextDouble();
+
+            Cartesiano pc = new Cartesiano(x1,y1,x2,y2);
+
+            System.out.print("\nO resultado dos pontos é: "+ pc.operacaopcartesiano());
+        }
+	}
 }
+        
+
+ 
