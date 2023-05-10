1031510fe32dca1009ec5c8c49ea858e97cb4023
Maria Luiza Ribeiro Rocha <106209553+Mariarocha0@users.noreply.github.com>
2023-05-10 20:03:56 +0000

    Criação da main

--- Marco1/Atividade2/Main.java
+++ Marco1/Atividade2/Main.java
@@ -1,5 +1,23 @@
 package Marco1.Atividade2;
 
 public class Main {
-    
+	public static void main(String[] args) {
+
+
+        Vetor vetor = new Vetor();
+        Aluno estudante = new Aluno("Fernando",21,8.2);
+        Aluno estudante2 = new Aluno("Bianca",23,10);
+        Aluno estudante3 = new Aluno("Letícia",20,6.0);
+
+        vetor.acrescenta(estudante);
+        vetor.acrescenta(estudante2);
+        vetor.acrescenta(estudante3);
+
+        System.out.println(vetor.tamanho());
+        System.out.println(vetor.contem(estudante));
+        System.out.println(vetor.contem(estudante2));
+        System.out.println(vetor.contem(estudante3));
+
+
+	}
 }
