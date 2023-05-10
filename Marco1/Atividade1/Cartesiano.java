f121768a3813fe9f756dc30ad318596b1f26b191
Maria Luiza Ribeiro Rocha <106209553+Mariarocha0@users.noreply.github.com>
2023-05-10 04:23:41 +0000

    Criação dos getters e setters

--- Marco1/Atividade1/Cartesiano.java
+++ Marco1/Atividade1/Cartesiano.java
@@ -10,5 +10,16 @@ public class Cartesiano {
     public Cartesiano(double x1, double x2, double y1, double y2){
         this.x1 = x1; this.x2 = x2; this.y1 = y1; this.y2 = y2; 
     }
-  
+    
+    private double getX1(){return x1;}
+    private double getX2(){return x2;}
+    private double getY1(){return y1;}
+    private double getY2(){return y2;}
+
+    private void setX1(double x1){this.x1 = x1;}
+    private void setX2(double x2){this.x2 = x2;}
+    private void setY1(double y1){this.y1 = y1;}
+    private void setY2(double y2){this.y2 = y2;}
+
+
 }
