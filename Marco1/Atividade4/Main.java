9a19bc6857e218372f49381b194e08aa84a73bac
Maria Luiza Ribeiro Rocha <106209553+Mariarocha0@users.noreply.github.com>
2023-05-12 19:14:00 +0000

    Atividade 4 completa

--- Marco1/Atividade4/Main.java
+++ Marco1/Atividade4/Main.java
@@ -1,5 +1,27 @@
 package Marco1.Atividade4;
 
 public class Main {
-    
+  
+    public static void main(String[] args) {
+
+        ILSE vetAlunos = new LSE();
+        Aluno estudantes1 = new Aluno("Kleber", 20, 6);
+        Aluno estudantes2 = new Aluno("Joana", 22, 8);
+        Aluno estudantes3 = new Aluno("Gabriele", 19, 10);
+
+        vetAlunos.insereFim(estudantes1);
+        vetAlunos.insereInicio(estudantes2);
+        vetAlunos.insereInicio(estudantes3);
+
+        System.out.println(vetAlunos.tamanho());
+
+        System.out.println(vetAlunos.estahVazia());
+
+        vetAlunos.remove(estudantes1);
+
+        System.out.println(vetAlunos.tamanho());
+
+        System.out.println(vetAlunos.imprimeLista());
+
+    }
 }
---
+++ Marco1/Atividade4/README.md
@@ -0,0 +1,7 @@
+**Atividade Prática 4**
+        Implemente um TAD LSE genérica:
+
+        
+        De modo que seja possível inserir Alunos ou quaisquer outros tipos de objetos.
+
+        Implemente um método para fazer a impressão da lista.
