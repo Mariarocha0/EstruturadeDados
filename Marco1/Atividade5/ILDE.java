6e50b8162ee05c2bc68a715e862dcb17596f6750
Maria Luiza Ribeiro Rocha <106209553+Mariarocha0@users.noreply.github.com>
2023-05-12 19:31:51 +0000

    Atividade 5 parcialmente concluida

---
+++ Marco1/Atividade5/ILDE.java
@@ -0,0 +1,11 @@
+package Marco1.Atividade5;
+public interface ILDE {
+
+    public void insereInicio(int info);
+    public void insereFim(int info);
+    public boolean estahVazia();
+    public boolean remove(int info);
+    public int tamanho();
+    public String imprimeListaInicio();
+    public String imprimeListaFim();
+}
