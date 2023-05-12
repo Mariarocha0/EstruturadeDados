30acd68512436dc71c3ce6d0b7cbb178f4233b97
Maria Luiza Ribeiro Rocha <106209553+Mariarocha0@users.noreply.github.com>
2023-05-12 19:44:43 +0000

    Atividade 6 pronta

--- Marco1/Atividade6/Main.java
+++ Marco1/Atividade6/Main.java
@@ -2,4 +2,24 @@ package Marco1.Atividade6;
 
 public class Main {
     
+    public static void main(String[] args) {
+
+        ILDE vetAlunos = new LDE();
+
+        Aluno estudantes1 = new Aluno("Kleber", 20, 6);
+        Aluno estudantes2 = new Aluno("Joana", 22, 8);
+        Aluno estudantes3 = new Aluno("Gabriele", 19, 10);
+    
+        System.out.println(vetAlunos.tamanho());
+    
+        System.out.println(vetAlunos.estahVazia());
+    
+        vetAlunos.remove(estudantes1);
+    
+        System.out.println(vetAlunos.tamanho());
+    
+        System.out.println(vetAlunos.imprimeListaInicio());
+        System.out.println(vetAlunos.imprimeListaFim());
+    
+    }
 }
