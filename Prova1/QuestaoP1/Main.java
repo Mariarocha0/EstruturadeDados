73d0a82fee32e9bcc7194e201a2150e76ee9e6fd
Maria Luiza Ribeiro Rocha <marialuiza.aluno@unipampa.edu.br>
2023-05-23 14:12:29 -0300

    Funcionouuuu

---
+++ Prova1/QuestaoP1/Main.java
@@ -0,0 +1,18 @@
+package Prova1.QuestaoP1;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Vetor numeros1 = new Vetor(10);
+    Vetor numeros2 = new Vetor(10);
+    numeros1.gerandoAleatorio();
+    numeros2.gerandoAleatorio();
+    numeros1.imprime();
+    System.out.println("------------------");
+    numeros2.imprime();
+
+    System.out.println(numeros1.getIntersecao(numeros2));
+    }
+
+    
+}
