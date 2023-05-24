00bfe69c82832cad8778c3cfe53b17acc9a930d4
Maria Luiza Ribeiro Rocha <marialuiza.aluno@unipampa.edu.br>
2023-05-24 03:18:33 -0300

    Questão 7 feitaaaa, organizando o repositório

---
+++ Prova1/QuestaoP7/Main.java
@@ -0,0 +1,17 @@
+package Prova1.QuestaoP7;
+
+public class Main {
+public static void main(String[] args) {
+    LDE lde = new LDE();
+
+    lde.insereFim(1);
+    lde.insereFim(2);
+    lde.insereFim(3);
+    lde.insereFim(4);
+    
+    System.out.println("Lista de recursão: ");
+    lde.imprime_rec();
+
+}
+
+}
---
+++ Prova1/QuestaoP7/Noh.java
@@ -0,0 +1,17 @@
+package Prova1.QuestaoP7;
+
+
+public class Noh {
+    
+    private Object info;
+    private Noh proximo;
+    
+    public Noh (Object info){
+    this.info = info;
+    this.proximo = null;
+    }
+    
+    public Object getInfo() {return this.info;}
+    public Noh getProximo() {return this.proximo;}
+    public Noh setProximo(Noh n) { return this.proximo = n; }
+}
