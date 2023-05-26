21a2955a552a3cec3ede455722d11197c94b8320
Maria Luiza Ribeiro Rocha <marialuiza.aluno@unipampa.edu.br>
2023-05-26 01:32:00 -0300

    Questão 8 da prova, feita!

--- Prova1/QuestaoP8/Main.java
+++ Prova1/QuestaoP8/Main.java
@@ -9,19 +9,22 @@ public class Main {
 		lista.insereFim(2);
 		lista.insereFim(3);
 		lista.insereFim(4);
-		System.out.println("List 1: ");
+        System.out.println("--------------------------");
+		System.out.println("Lista 1: ");
 		lista.imprimeListaInicio();
 
 		lista2.insereFim(5);
 		lista2.insereFim(6);
 		lista2.insereFim(7);
 		lista2.insereFim(8);
-		System.out.println("List 2: ");
+        System.out.println("--------------------------");
+		System.out.println("Lista 2: ");
 		lista2.imprimeListaInicio();
 		
 		lista.concat(lista2);
 
-		System.out.println("List concatenated: ");
+        System.out.println("--------------------------");
+		System.out.println("Lista Completa: ");
 		lista.imprimeListaInicio();
 		
 	}
