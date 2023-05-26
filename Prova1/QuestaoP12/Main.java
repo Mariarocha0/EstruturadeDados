106c5eadd58fec9d8f5aba11dde29e0611cba7fc
Maria Luiza Ribeiro Rocha <marialuiza.aluno@unipampa.edu.br>
2023-05-26 16:05:04 -0300

    Questão 12, feitaaaa!!

--- Prova1/QuestaoP12/Main.java
+++ Prova1/QuestaoP12/Main.java
@@ -1,5 +1,16 @@
 package Prova1.QuestaoP12;
+import java.util.Scanner;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        Recursao recursao = new Recursao();
+        System.out.println("Recursão:");
+        System.out.print("Digite um número inteiro: ");
+        int num = entrada.nextInt();
+
+        recursao.mostrarSoma(num);
+
+    }
 }
